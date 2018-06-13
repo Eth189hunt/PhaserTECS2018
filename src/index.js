5b20ad142b194583b68118601c1bc18e4eddac46
temp user3 <tempuser3@SUU.EDU>
2018-06-12 18:29:53 -0600

    Player ship Movment and Fire

--- src/index.js
+++ src/index.js
@@ -8,7 +8,16 @@ var config = {
     scene: {
         preload: preload,
         create: create,
-		update: update
+		update: update,
+		extend: {
+			cursors: null,
+			thrust: null,
+			flares: null,
+			bullets: null,
+			lastFired: 0,
+			text: null,
+			createBulletEmitter: createBulletEmitter
+		}
     }
 };
 
@@ -24,58 +33,83 @@ var me = null;
 var bad = null;
 var laser = null;
 var cursors;
+
 function create ()
 {
     this.me = this.add.sprite(400, 550, 'ship');
 	this.bad = this.add.sprite(400, 300, 'shipB');
 	this.laser = this.add.group();
-	this.laser.enableBody = true;
-    this.laser.physicsBodyType = Phaser.Physics.ARCADE;
 	
-	this.laser.displayWidth = 10;
-	this.laser.displayHeight = 30;
-	
-    for (var i = 0; i < 20; i++)
-    {
-        var b = this.laser.create(400, 550, 'laser');
-        b.name = 'laser' + i;
-		b.displayWidth = 10;
-		b.displayHeight = 30;
-        b.exists = false;
-        b.visible = false;
-        b.checkWorldBounds = true;
-    }
 	
 	this.me.displayWidth = 80; 
 	this.me.displayHeight = 80;
 	this.me.setAngle(180);
 	console.log(this.me);
-	this.input.on('pointermove', function (pointer) {
-
-        this.me.x = Phaser.Math.Clamp(pointer.x, 52, 748);
-
-        }, this);
 	
 	this.bad.displayWidth = 80; 
 	this.bad.displayHeight = 80;
 	this.bad.setAngle(180);
+	var Bullet = new Phaser.Class({
+		Extends: Phaser.GameObjects.Image,
+		
+		initialize:
+		
+		function Bullet (scene){
+			Phaser.GameObjects.Sprite.call(this, scene, 0,0, 'laser');
+			this.displayWidth = 20;
+			this.displayHeight = 50;
+			
+			this.speed = 0;
+			this.born = 0;
+		},
+		fire: function (me){
+			this.born = 0;
+			this.setPosition(me.x,me.y);
+			
+			this.speed = Phaser.Math.GetSpeed(500,-1);
+			//this.velocity.y = -300;
+		},
+		update: function (time, delta){
+			this.y += this.speed * delta;
+			
+			this.born += delta;
+			
+			if (this.born > 1000){
+				this.setActive(false);
+				this.setVisible(false);
+			}
+		}
+	})
+	this.createBulletEmitter();
+	//bullets
+	this.bullets = this.add.group({classType: Bullet, runChildUpdate: true})
 	
-    game.input.keyboard.addKeyCapture([ Phaser.Keyboard.SPACEBAR ]);	
-	
+    this.cursors = this.input.keyboard.createCursorKeys();	
+	console.log("cddgdghjgbooo");
+	this.input.on('pointermove', function (pointer) {
+		
+		this.me.x = Phaser.Math.Clamp(pointer.x, 52, 748);
+		
+    }, this);
 }
-function update(){
-	/*if(game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)){
-		fireLaser();
-	}*/
+function update(time, delta){
+	if(this.cursors.space.isDown && time > this.lastFired){
+		var bullet = this.bullets.get();
+		bullet.setActive(true);
+		bullet.setVisible(true);
+		if(bullet){
+			bullet.fire(this.me);
+			
+			this.lastFired = time + 175;
+		}
+	}
+}
+function createBulletEmitter(){
+	
 }
 function fireLaser(){
-        l = this.laser.getFirstExists(false);
-
-        if (l)
-        {
-            l.reset(sprite.x + 6, sprite.y - 8);
-            l.body.velocity.y = -300;
-        }
+console.log("booo");
+    
 }
 function resetLaser(){
 	leaser.kill();
